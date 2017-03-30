fc2f7652b71a2bc814355e1bc83eccccecb2005c
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2017-03-30 19:13:21 -0400

    well I'm silly

--- lib/sequence_diagram.rb
+++ lib/sequence_diagram.rb
@@ -1,22 +1,3 @@
-module SequenceDiagram
-  def convert_mermaid_to_png(content)
-    uid = Digest::MD5.hexdigest(content)
-    file = Tempfile.new([uid, '.mmd'])
-    file.write(content)
-    file.flush
-
-    output = `mermaid #{file.path} --png --outputDir #{Application.constants[:TEMP_MERMAID]}`
-    file.close
-    return [false, output] if $?.exitstatus != 0
-
-    files = Dir["#{Application.constants[:TEMP_MERMAID]}/#{uid}*.png"]
-    Application.logger.info "Mermaid made a file at #{files.inspect}"
-    return [false, output] if files.empty?
-
-    [true, files.first]
-  end
-end
-
 require 'charts'
 
 module SequenceDiagram
@@ -56,5 +37,5 @@ module SequenceDiagram
     return [false, output] if files.empty?
 
     [true, files.first]
-  en
+  end
 end
